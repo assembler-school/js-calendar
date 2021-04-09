0047db0762d3cd60bb4ca174291463cce374e9ac
Armando <armandovieirag@gmail.com>
2021-04-09 18:34:53 +0200

    F05 Animatio

--- src/assets/js/calendar.js
+++ src/assets/js/calendar.js
@@ -1,5 +1,4 @@
 const date = new Date();
-
 /* * Set the current date in header */
 function setCurrentDate () {
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
@@ -11,9 +10,8 @@ setCurrentDate();
  * set calendar
  * @ Author: Armando
  */
-const renderCalendar = () => {
+const renderCalendar = (direction) => {
     date.setDate(1);
-
     const monthDays = document.querySelector(".days-wrapper");
 /* Gets total days of current month */
     const lastDay = new Date(
@@ -58,35 +56,35 @@ const renderCalendar = () => {
     let days = "";
     // creating div with prev days of calendar
     for (let x = firstDayIndex; x > 0; x--) {
-        days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+        days += `<div class="prev-date ${direction}"><div class = "next-date day day-number">${prevLastDay - x + 1}</div></div>`;
     }
     // creating div with days of calendar
     for (let i = 1; i <= lastDay; i++) {
         if (
             i === new Date().getDate() &&
-            date.getMonth() === new Date().getMonth()
+            date.getMonth() === new Date().getMonth() &&
+            date.getFullYear() === new Date().getFullYear()
         ) {
-            days += `<div class="day today"><div class = "day-number">${i}</div></div>`;
+            days += `<div class="day today ${direction}"><div class = "day-number">${i}</div></div>`;
         } else {
-            days += `<div class = "day"><div class = "day-number">${i}</div></div>`;
+            days += `<div class = "day ${direction}"><div class = "day-number">${i}</div></div>`;
         }
     }
 // creating div with next days of calendar
     for (let j = 1; j <= nextDays; j++) {
-        days += `<div class="next-date">${j}</div>`;
+        days += `<div class="${direction}"><div class = "next-date day day-number">${j}</div></div>`;
     }
     /* Injecting all elements to DOM */
     monthDays.innerHTML = days;
 };
-
 document.querySelector("#prevMonth").addEventListener("click", () => {
     date.setMonth(date.getMonth() - 1);
-    renderCalendar();
+    renderCalendar("left");
 });
 
 document.querySelector("#nextMonth").addEventListener("click", () => {
     date.setMonth(date.getMonth() + 1);
-    renderCalendar();
+    renderCalendar("right");
 });
 
-renderCalendar();
+renderCalendar("");
