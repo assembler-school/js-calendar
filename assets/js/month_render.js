8c1c198884feda6794b12fb6cb6fcc408b835bdc
Jordi <aejordi@gmail.com>
2021-04-09 13:44:01 +0200

    F02-Iteration function done

--- assets/js/month_render.js
+++ assets/js/month_render.js
@@ -7,10 +7,28 @@ function renderMonth(selectedDate) {
     let month = selectedDate.getMonth();
     let year = selectedDate.getFullYear();
     let firstDay = (new Date(year, month)).getDay();
-    console.log(firstDay);
-    let dayOne = document.querySelector('#first__week div[data-col="' + firstDay + '"]');
     let daysInMonth = 32 - new Date(year, month, 32).getDate();
-    console.log(daysInMonth);
+
+    let dayOne = document.querySelector('#first__week div[data-col="' + firstDay + '"]');
+    console.log(dayOne);
+    let weekDay = firstDay;
+    let weeks = document.getElementsByClassName('week');
+    console.log(weeks[0]);
+    let weekCount = 1;
+
+    for (let x = 1; x < daysInMonth + 1; x++) {
+        if (weekDay == 0) {
+            document.querySelector('.week:nth-child(' + weekCount + ') div[data-col="' + weekDay + '"]').innerHTML = x;
+            weekCount++;
+            weekDay++;
+        } else if (weekDay == 6) {
+            document.querySelector('.week:nth-child(' + weekCount + ') div[data-col="' + weekDay + '"]').innerHTML = x;
+            weekDay = 0;
+        } else {
+            document.querySelector('.week:nth-child(' + weekCount + ') div[data-col="' + weekDay + '"]').innerHTML = x;
+            weekDay++;
+        }
+    }
 }
 
 renderMonth(date);
