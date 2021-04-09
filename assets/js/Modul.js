7d2a432dad70a09cb15257adeaf723435bf948e7
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2021-04-09 15:43:08 +0200

    Modal finished without final style

--- assets/js/Modul.js
+++ assets/js/Modul.js
@@ -1,7 +1,22 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "flex";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
+
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("openButton");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+openButton.onclick = function() {
+  modal.style.display = "flex";
+}
+
+function closeForm() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+// Set current date on Caledar 
+let today = new Date().toISOString().substr(0, 10);
+document.querySelector("#initialCal").value = today;
