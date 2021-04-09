8d2a0c62c3d5ced51f2141f39522e42cfcb6bbd6
Ernest-DT <ernest.duocastella@gmail.com>
2021-04-09 15:11:22 +0200

    f02 - cleanerer code

--- assets/js/month_render.js
+++ assets/js/month_render.js
@@ -6,25 +6,18 @@ console.log(actualDate);
 function renderMonth(selectedDate) {
     let month = selectedDate.getMonth();
     let year = selectedDate.getFullYear();
-    /* Get the first day of month and number of days in month*/
+    /* Get the first day of month and number of days in month */
     let firstDay = (new Date(year, month)).getDay();
     let daysInMonth = 32 - new Date(year, month, 32).getDate();
     /* Variable declarations for loop */
     let weekDay = firstDay;
     let weekCount = 1;
-    /* Loop to fill calendar*/
+    /* Loop to fill calendar */
     for (let x = 1; x < daysInMonth + 1; x++) {
-        if (weekDay == 0) {
-            document.querySelector('.week:nth-child(' + weekCount + ') div[data-col="' + weekDay + '"]').innerHTML = x;
-            weekCount++;
-            weekDay++;
-        } else if (weekDay == 6) {
-            document.querySelector('.week:nth-child(' + weekCount + ') div[data-col="' + weekDay + '"]').innerHTML = x;
-            weekDay = 0;
-        } else {
-            document.querySelector('.week:nth-child(' + weekCount + ') div[data-col="' + weekDay + '"]').innerHTML = x;
-            weekDay++;
-        }
+        document.querySelector('.week:nth-child(' + weekCount + ') div[data-col="' + weekDay + '"]').innerHTML = x;
+        if (!weekDay) {weekCount++};
+        weekDay++;
+        weekDay%=7;
     }
 }
 
