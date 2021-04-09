9cbc844c20d6b4e440b9684fb0ca08da4677f621
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2021-04-09 17:57:19 +0200

    Modal checkbox functions done

--- assets/js/Modul.js
+++ assets/js/Modul.js
@@ -20,3 +20,29 @@ function closeForm() {
 // Set current date on Caledar 
 let today = new Date().toISOString().substr(0, 10);
 document.querySelector("#initialCal").value = today;
+
+
+
+// Show calendar when checkbox checked
+let checkboxEnd = document.getElementById("checkboxEnd")
+let endDate = document.getElementById("endCal");
+
+function displayEnd () {
+  if(checkboxEnd.checked == true) {
+      endDate.style.display = "block"
+  } else {
+    endDate.style.display = "none"
+  }
+} // End Function
+
+// Show Remind me select when checked
+let reminderCheckBox = document.getElementById("reminderCheckBox");
+let reminderTextArea = document.getElementById("reminderSelect");
+
+function reminderShowTextArea() {
+  if (reminderCheckBox.checked == true) {
+    reminderTextArea.style.display = "flex"
+  } else {
+    reminderTextArea.style.display = "none"
+  }
+} // End function
