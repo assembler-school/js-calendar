eded773620049aa0bc15b7c8ca1824f3fb870e9b
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-04-12 12:40:11 +0200

    Finished event modal main functionality

--- src/assets/js/event-modal.js
+++ src/assets/js/event-modal.js
@@ -11,14 +11,38 @@ function eventModal (event) {
     /* 
     TODO it has to load the data inside the modal
    */
-  //* Showing the modal
+    //* constants in use
     const modalBackground = document.getElementById("modal-event-section");
+    const initialDate = document.getElementById("modal-event-initial-date");
+    const endDate = document.getElementById("modal-event-end-date");
+    const reminder = document.getElementById("modal-event-reminder");
+    const eventType = document.getElementById("modal-event-type");
+    const description = document.getElementById("modal-event-description");
 
-    //* Adding eventListener to modal
-    
+     //* Adding eventListener to modal
+    document.getElementById("modal-event-close-button").addEventListener('click', closeEventModal);
+    document.getElementById("modal-event-edit-btn").addEventListener('click', editEventModal);
+    //* Adding content to modal
+    //TODO
+    //* Showing the modal
     modalBackground.classList.remove("hidden");
 }
 
+
+//* Close event modal function
+function closeEventModal () {
+    const modalBackground = document.getElementById("modal-event-section");
+    //* Remove event listeners
+    document.getElementById("modal-event-close-button").removeEventListener('click', closeEventModal);
+    document.getElementById("modal-event-edit-btn").removeEventListener('click', editEventModal);
+    modalBackground.classList.add("hidden");
+}
+
+//* Edit event modal function
+function editEventModal () {
+    //! TODO has to open normal modal and insert this event values
+    closeEventModal();
+}
 // ! Function to add a mock event
 
 function addEventTesting () {
