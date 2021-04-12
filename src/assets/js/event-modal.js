134fb8aa38b6c0041ebcbf39ce4fa082a00384bb
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-04-12 09:18:48 +0200

    main function declaration

---
+++ src/assets/js/event-modal.js
@@ -0,0 +1,17 @@
+
+/*! This function is to be executed each time someone click on an event
+TODO associate eventListener
+*/
+
+function eventModal (event) {
+    /* 
+    TODO Here it has to include some code to get the information from localStorage 
+    */
+   /* 
+   TODO it has to include the data inside the modal
+   */
+  //* Showing the modal
+    const modalBackground = document.getElementById("modal-event-section");
+
+    modalBackground.classList.remove("hidden");
+}
