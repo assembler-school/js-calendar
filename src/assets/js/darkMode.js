6b5c235526bf13aeb68bbfda224d15f235b42c17
Ethan Alfaro <ctabbx@gmail.com>
2021-04-16 17:25:36 +0200

    R11

--- src/assets/js/darkMode.js
+++ src/assets/js/darkMode.js
@@ -1,14 +1,30 @@
+const storage = window.localStorage;
 const cal = document.getElementById('main');
 const Darkdays = document.getElementById('modal-event-div');
 const checkboxDm = document.getElementById('checkboxDM');
 
+const intializeDarkMode =() =>{
+    let darkmodeEnabled =
+    localStorage.getItem('checked') ? JSON.parse(localStorage.getItem('checked')) : false;
+    if(darkmodeEnabled){
+        cal.classList.add('backgrounDark');
+        Darkdays.classList.add('colorDark');
+        storage.setItem('checked',true);
+        checkboxDm.checked = true;
+    }
+}
+intializeDarkMode();
 const checkedFunction = () =>{
     if(document.getElementById('checkboxDM').checked){
     cal.classList.add('backgrounDark');
     Darkdays.classList.add('colorDark');
+    storage.setItem('checked',true);
 }else{
     cal.classList.remove('backgrounDark');
     Darkdays.classList.remove('colorDark');
+    storage.setItem('checked',false);
 }
 }
 checkboxDm.addEventListener('click',checkedFunction);
+
+
