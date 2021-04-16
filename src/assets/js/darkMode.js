4937e7b0ca06657ba6bd9c556c30a620136d7996
Armando <armandovieirag@gmail.com>
2021-04-16 13:31:09 +0200

    dev Adding colordark to weekdays

--- src/assets/js/darkMode.js
+++ src/assets/js/darkMode.js
@@ -1,14 +1,17 @@
 const cal = document.getElementById('main');
 const Darkdays = document.getElementById('modal-event-div');
 const checkboxDm = document.getElementById('checkboxDM');
+const darkWeek = document.querySelector(".weekdays");
 
 const checkedFunction = () =>{
     if(document.getElementById('checkboxDM').checked){
     cal.classList.add('backgrounDark');
     Darkdays.classList.add('colorDark');
+    darkWeek.classList.add("colorDark")
 }else{
     cal.classList.remove('backgrounDark');
     Darkdays.classList.remove('colorDark');
+    darkWeek.classList.add("colorDark")
 }
 }
 checkboxDm.addEventListener('click',checkedFunction);
