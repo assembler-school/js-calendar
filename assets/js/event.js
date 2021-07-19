f91e6e63645c1f859f0adfd858d23c587d0978d5
Cherrerotinoco <c.herrerotinoco@gmail.com>
2021-07-19 18:00:10 +0200

    Add new listener to modal

--- assets/js/event.js
+++ assets/js/event.js
@@ -9,12 +9,14 @@ function renderAddEventForm() {
             <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.      
             </p>
+            <button id="modal__cancel" class="modal__cancel">Cancel</button>
         </div>
     `;
 
     // To close the current Modal
     document.getElementById('modal__close').addEventListener('click', closeModal);
     document.getElementById('modal__overlay').addEventListener('click', closeModal);
+    document.getElementById('modal__cancel').addEventListener('click', closeModal);
     document.addEventListener('keydown', e => {
         if (e.key === 'Escape') closeModal();
     })
