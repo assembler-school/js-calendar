ae04880f27ea40f0191cc1c081506cbcbd5e7797
tamtran <tamtran2885@gmail.com>
2021-07-20 17:02:29 +0200

    form of modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,41 @@
 // ----------- VARIABLES --------
+
+// Modal
 let buttonEvent = document.querySelector("#calendarEvent");
 let modal = document.querySelector("#modal");
 let modalClose = document.querySelector("#button-close");
+let closeModalBtn = document.querySelector("#close-modal");
+
+// Form
+const endDate = document.getElementById("endDate");
+const remind = document.getElementById("remind");
+const endDateInput = document.getElementById("endDateInput");
+const remindInput = document.getElementById("remindInput");
 
 // --------- EVENT LISTENER --------
 
 buttonEvent.addEventListener("click", openModal);
 modalClose.addEventListener("click", closeModal);
+closeModalBtn.addEventListener("click", closeModal);
 
 function openModal() {
-  modal.classList.add("modal--is-visible");
+  modal.classList.add("--is-visible");
 }
 
 function closeModal() {
-  modal.classList.remove("modal--is-visible");
+  modal.classList.remove("--is-visible");
+}
+
+
+// --------- FORM VALIDATION --------
+endDate.addEventListener("click", displayStartDate);
+remind.addEventListener("click", displayRemindEvent);
+
+function displayStartDate() {
+  endDateInput.classList.toggle("--is-hidden");
+}
+
+function displayRemindEvent() {
+  // remindInput.classList.replace("--is-hidden", "--is-visible");
+  remindInput.classList.toggle("--is-hidden");
 }
