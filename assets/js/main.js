b92b78c0478fe997d2bb649a28bf4ce271e7d15f
Enrique Hidalgo <koko.freelance@gmail.com>
2021-07-20 13:46:07 +0100

    fixed modal functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,19 @@
 // ----------- VARIABLES --------
 let buttonEvent = document.querySelector("#calendarEvent");
-let modal = document.querySelector('#modal');
-let modalClose = document.querySelector('#button-close')
+let modal = document.querySelector("#modal");
 
-// --------- EVENT LISTENER --------
+// --------- EVENT LISTENERS FOR EVENT BUTTON & MODAL --------
 
 buttonEvent.addEventListener("click", openModal);
-// modalClose.addEventListener("click", closeModal);
 
 function openModal() {
-  modal.classList.add('modal--is-visible')
+  modal.classList.add("modal--is-visible");
 }
 
+let modalClose = document.querySelector("#button-close");
+modalClose.addEventListener("click", closeModal);
+
+function closeModal() {
+  modal.classList.remove("modal--is-visible");
+}
+// _______OPEN & CLOSE ___________
