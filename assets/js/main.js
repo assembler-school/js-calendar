1744725e15920ba0bd0e9465be1df5249a24e42f
AranBeitia <arantza.beitia@gmail.com>
2021-07-20 13:27:17 +0200

    Show modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,14 @@
 // ----------- VARIABLES --------
 let buttonEvent = document.querySelector("#calendarEvent");
+let modal = document.querySelector('#modal');
+let modalClose = document.querySelector('#button-close')
 
 // --------- EVENT LISTENER --------
 
 buttonEvent.addEventListener("click", openModal);
+// modalClose.addEventListener("click", closeModal);
 
 function openModal() {
-  console.log("Hello Tam!!");
+  modal.classList.add('modal--is-visible')
 }
+
