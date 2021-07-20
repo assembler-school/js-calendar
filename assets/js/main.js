6f9b8c034f2aaef0814ad6b320f59b327638457a
Enrique Hidalgo <koko.freelance@gmail.com>
2021-07-20 09:40:47 +0100

    Button header half functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-console.log("Hello Kitty");
+// ----------- VARIABLES --------
+let buttonEvent = document.querySelector("#calendarEvent");
+
+// --------- EVENT LISTENER --------
+
+buttonEvent.addEventListener("click", openModal);
+
+function openModal() {
+  console.log("Hello Tam!!");
+}
