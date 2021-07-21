8825947f65f8c9f9518a317be6b75a57ac14b879
Daniella Alamo <dalamov08@gmail.com>
2021-07-21 09:31:21 +0200

    Changes to the style and js

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,19 +1,73 @@
 'use strict';
 
-showCalendar = (month, year) => { }
+const writeMonth = (month) => {
 
-getDaysInMonth = (month) => { }
+    for(let i = startDay(); i>0;i--){
+        dates.innerHTML += ` <div class="calendar__date">
+            ${getTotalDays(monthNumber-1)-(i-1)} </div>`;
+    }
 
-getCalendarLimit = (params) => { }
+    for(let i=1; i<=getTotalDays(month); i++){
+        if(i===currentDay) {
+            dates.innerHTML += ` <div class="calendar__date">${i}</div>`;
+        }else{
+            dates.innerHTML += ` <div class="calendar__date">${i}</div>`;
+        }
+    }
+}
 
-writeMonth = (month) => { }
+const getTotalDays = month => {
+    if(month === -1) month = 11;
 
-startDay = () => { }
+    if (month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11) {
+        return  31;
 
-isLeap = () => { }
+    } else if (month == 3 || month == 5 || month == 8 || month == 10) {
+        return 30;
 
-setNewDate = () => { }
+    } else {
 
-getNextMonth = (month) => { }
+        return isLeap() ? 29:28;
+    }
+}
 
-getPrevMonth = (month) => { }
+const isLeap = () => {
+    return ((currentYear % 100 !==0) && (currentYear % 4 === 0) || (currentYear % 400 === 0));
+}
+
+const startDay = () => {
+    let start = new Date(currentYear, monthNumber, 1);
+    return ((start.getDay()-1) === -1) ? 6 : start.getDay()-1;
+}
+
+const lastMonth = () => {
+    if(monthNumber !== 0){
+        monthNumber--;
+    }else{
+        monthNumber = 11;
+        currentYear--;
+    }
+
+    setNewDate();
+}
+
+const nextMonth = () => {
+    if(monthNumber !== 11){
+        monthNumber++;
+    }else{
+        monthNumber = 0;
+        currentYear++;
+    }
+
+    setNewDate();
+}
+
+const setNewDate = () => {
+    currentDate.setFullYear(currentYear,monthNumber,currentDay);
+    month.textContent = monthNames[monthNumber];
+    year.textContent = currentYear.toString();
+    dates.textContent = '';
+    writeMonth(monthNumber);
+}
+
+writeMonth(monthNumber);
