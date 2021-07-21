6f247b76896bede9a55380b8ab752140c83f43b1
Pere Serra Bosch <pere.serra.bosch@gmail.com>
2021-07-21 17:36:30 +0200

    fix change month buttons

--- assets/js/variables.js
+++ assets/js/variables.js
@@ -5,8 +5,8 @@ const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'Jul
 
 const currentDate = new Date();
 const currentDay = currentDate.getDate();
-const currentMonth = currentDate.getMonth();
-const currentYear = currentDate.getFullYear();
+var currentMonth = currentDate.getMonth();
+var currentYear = currentDate.getFullYear();
 
 let dates = document.getElementById('dates');
 let month = document.getElementById('month');
