b90e663904d64918fb0d05ff87272da67d3c3699
Jose1i1o <guadix88@gmail.com>
2021-07-21 19:23:25 +0200

    add positivism vibe(sentences)s to the calendar

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@
 
 import { displayModal } from "./modal.js";
 import { renderCalendar } from "./calendar.js";
+import { getRandomQuote } from "./quotes-database.js";
 
 /* INVOKING BASE FUNCTIONS ON LOAD PAGE */
 renderCalendar(0);
+getRandomQuote();
