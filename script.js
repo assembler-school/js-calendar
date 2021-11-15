db8b870254b5f7c5ad1624c57149726dc25face7
Bertinoti <jeffersonbertinoti@gmail.com>
2021-11-15 12:32:13 +0100

    commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function displayCalendar(){
+function displayCalendar() {
 
 
-    var htmlContent ="";
-    var FebNumberOfDays ="";
+    var htmlContent = "";
+    var FebNumberOfDays = "";
     var counter = 1;
 
 
@@ -11,49 +11,49 @@ function displayCalendar(){
     var month = dateNow.getMonth();
     var day = dateNow.getDate();
     var year = dateNow.getFullYear();
-    var nextMonth = month+1;
+    var nextMonth = month + 1;
     // var prevMonth = month -1;
 
 
 
     //Determing if February (28,or 29)
-    if (month == 1){
-        if ( (year%100!=0) && (year%4==0) || (year%400==0)){
+    if (month == 1) {
+        if ((year % 100 != 0) && (year % 4 == 0) || (year % 400 == 0)) {
             FebNumberOfDays = 29;
-        }else{
+        } else {
             FebNumberOfDays = 28;
         }
     }
 
 
     // names of months and week days.
-    var monthNames = ["January","February","March","April","May","June","July","August","September","October","November", "December"];
-    var dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thrusday","Friday", "Saturday"];
-    var dayPerMonth = ["31", ""+FebNumberOfDays+"","31","30","31","30","31","31","30","31","30","31",];
+    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday"];
+    var dayPerMonth = ["31", "" + FebNumberOfDays + "", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31",];
 
 
     // days in previous month and next one , and day of week.
-    var nextDate = new Date(nextMonth +' 1 ,'+year);
-    var weekdays= nextDate.getDay();
+    var nextDate = new Date(nextMonth + ' 1 ,' + year);
+    var weekdays = nextDate.getDay();
     var weekdays2 = weekdays;
     var numOfDays = dayPerMonth[month];
 
 
 
-    
+
     // this leave a white space for days of pervious month.
-    while (weekdays>0){
+    while (weekdays > 0) {
         htmlContent += "<li class='monthPre'></li  >";
         weekdays--;
-        
 
-    }  
-   
+
+    }
+
     // loop to build the calendar body.
-    while (counter <= numOfDays){
+    while (counter <= numOfDays) {
 
         // When to start new line.
-        if (weekdays2 > 6){
+        if (weekdays2 > 6) {
             weekdays2 = 0;
             htmlContent += "</ul><ul> ";
         }
@@ -62,14 +62,14 @@ function displayCalendar(){
 
         // if counter is current day.
         // highlight current day using the CSS defined in header.
-        if (counter == day){
-            htmlContent +="<li class='dayNow'  onMouseOver='this.style.background=\"#FFFF00\"; this.style.color=\"#FFFFFF\"' "+
-                "onMouseOut='this.style.background=\"#FFFFFF\"; this.style.color=\"#00FF00\"'>"+counter+"</li>";
-        }else {
-            htmlContent +="<li class='monthNow' onMouseOver='this.style.background=\"#FFFF00\"'"+
-                " onMouseOut='this.style.background=\"#FFFFFF\"'>"+counter+"</li>";
+        if (counter == day) {
+            htmlContent += "<li class='dayNow'  onMouseOver='this.style.background=\"#FFFF00\"; this.style.color=\"#FFFFFF\"' " +
+                "onMouseOut='this.style.background=\"#FFFFFF\"; this.style.color=\"#00FF00\"'>" + counter + "</li>";
+        } else {
+            htmlContent += "<li class='monthNow' onMouseOver='this.style.background=\"#FFFF00\"'" +
+                " onMouseOut='this.style.background=\"#FFFFFF\"'>" + counter + "</li>";
 
-        } 
+        }
 
         weekdays2++;
         counter++;
@@ -79,13 +79,13 @@ function displayCalendar(){
 
     // building the calendar html body.
     var calendarBody = "<div class='calendar'> <ul class='monthNow'><li colspan='7'>"
-        +monthNames[month]+" "+ year +"</li></ul>";
-    calendarBody +="<ul class='dayNames'>  <li>Sun</li>  <li>Mon</li> <li>Tues</li>"+
+        + monthNames[month] + " " + year + "</li></ul>";
+    calendarBody += "<ul class='dayNames'>  <li>Sun</li>  <li>Mon</li> <li>Tues</li>" +
         "<li>Wed</li> <li>Thurs</li> <li>Fri</li> <li>Sat</li> </ul>";
     calendarBody += "<ul>";
     calendarBody += htmlContent;
     calendarBody += "</ul></div>";
     // set the content of div .
-    document.getElementById("calendar").innerHTML=calendarBody;
+    document.getElementById("calendar").innerHTML = calendarBody;
 
 }
