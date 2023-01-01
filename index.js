0195f6aec5e1ed936b36b862bb6a8f9aaaefcb9f
Antonio Rufino <devs.toni@gmail.com>
2023-01-01 13:01:39 +0100

    add modal with not final form

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const addEvent = () => {
-  
+
+// OPEN MODAL
+const isVisible = "is-visible";
+const openModal = document.querySelector("[data-open]");
+openModal.addEventListener("click", function () {
+  const modalId = this.dataset.open;
+  document.getElementById(modalId).classList.add(isVisible);
+});
+
+// CLOSE MODAL
+const closeModal = document.querySelectorAll("[data-close]");
+
+for (const el of closeModal) {
+  el.addEventListener("click", function() {
+    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+  });
 }
-
-document.getElementById('btnAdd').addEventListener("click", addEvent);
-
+document.addEventListener("click", e => {
+  if (e.target == document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+document.addEventListener("keyup", e => {
+  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
